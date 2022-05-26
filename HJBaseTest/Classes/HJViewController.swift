aeb39f60884d87517c09dcff752d69b5f8108fe1
hanwanjie <hanwanjie@jeejio.com>
2022-05-26 18:16:24 +0800

    添加测试代码

---
+++ HJBaseTest/Classes/HJViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HJViewController.swift
+//  HJBaseTest_Example
+//
+//  Created by 王木木 on 2022/5/26.
+//  Copyright © 2022 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class HJViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func testPrint() {
+        print("哈哈哈，我的pod的打印")
+    }
+}
+
